1db400b68f3779bf8b27266694ff195be41bad5b
tc3158494-cpu <tc3158494@gmail.com>
2025-12-23 00:58:43 +0530

    transformation.py

--- setup.py
+++ setup.py
@@ -1,9 +1,21 @@
 from setuptools import find_packages, setup
+from typing import list
+HYPEN_E_DOT= '-e.'
+def get_requirement(file_path:str)-> list[str]:
+    requirement=[]
+    with open(file_path) as file_obj:
+        requirement= file_obj.readlines()
+        requirement=[ req.replace("\n"," ") for req in requirement]
+        if HPEN_E_DOT in requirement :
+            requirement.remove(HPEN_E_DOT)
+    return requirement
+     
+
 setup (
     name = 'my first ML project',
     version= '0.0.1',
     author= ' tushar',
     author_email= ' tc3158494@gmail.com',
     packages = find_packages(),
-    install_requires= get_requirment('requirement.txt')
+    install_requires= get_requirement('requirement.txt')
 )
